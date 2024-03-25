c22bf30b5ffc90d7a1c480cb267ec41635af1e86
uexle <wesleivianna235@gmai.com>
2024-03-24 23:32:38 -0300

    product loc

--- script.js
+++ script.js
@@ -5,36 +5,42 @@ const productsData = [
         imgSrc: 'https://static-01.daraz.com.bd/p/ed9e2c44893981a1a81f9061eda0189e.jpg',
         description: 'Fonte',
         code: '12345678',
+        loc: 'P01.1TA1',
         tags: ['ele']
     },
     {
         imgSrc: 'https://cdn.leroymerlin.com.br/products/parafuso_area_molhada_4,8x38mm_auto_atarraxante_philips_10un_86941491_dfdc_600x600.jpg',
         description: 'Parafuso',
         code: '23456789',
+        loc: 'P01.1TA1',
         tags: ['pap']
     },
     {
         imgSrc: 'https://daxonart8aj4m.cloudfront.net/Custom/Content/Products/72/89/7289_34113-culos-de-proteo-jaguar-incolor-linha-epi-kalipso_m1_637853711153724536.jpg',
         description: 'Óculos de Proteção',
         code: '34567890',
+        loc: 'P01.1TA1',
         tags: ['epi']
     },
     {
         imgSrc: 'https://tb0932.vtexassets.com/arquivos/ids/163205/117792.png?v=637705340721670000',
         description: 'Vassoura Azul',
         code: '45678901',
+        loc: 'P01.1TA1',
         tags: ['lim']
     },
     {
         imgSrc: 'https://centermedical.vtexassets.com/arquivos/ids/164092/Protetor-Auricular-de-Silicone---Supermedy---Tipo-Plug---Center-Medical.png?v=637992988521700000',
         description: 'Protetor Auricular',
         code: '56789012',
+        loc: 'P01.1TA1',
         tags: ['epi']
     },
     {
         imgSrc: 'https://a-static.mlcdn.com.br/450x450/eixo-virabrequim-do-motor-chevrolet-onix-prisma-celta-corsa-classic-novo-corsa-todos-1-0-8v-flex-original-chevrolet/altese/2010796/637f91df5a01474e7107d7c4b7e29303.jpeg',
         description: 'Eixo Virabrequim',
         code: '67890123',
+        loc: 'P01.1TA1',
         tags: ['mec']
     },
 ];
@@ -49,11 +55,15 @@ function generateProducts() {
         let productImg = product.querySelector('img');
         let productDescription = product.querySelector('.description');
         let productCode = product.querySelector('.code');
+        let productCode1 = product.querySelector('.code1');
+        let productLoc = product.querySelector('.loc');
         let productTags = product.querySelector('tags');
 
         productImg.src = data.imgSrc;
         productDescription.textContent = data.description;
-        productCode.innerHTML = `COD.<span>${data.code}</span>`
+        productCode.innerHTML = `DE<span>${data.code}</span>`
+        productCode1.innerHTML = `PARA<span>${data.code}</span>`
+        productLoc.innerHTML = `RUA:<span>${data.loc}</span>`
         productsContainer.appendChild(product);
         productTags.textContent = data.tags.join(',');
     });
